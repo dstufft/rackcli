ef7360a93c9f948434e51b1cb2d78a8dbb853dfc
Jesse Noller <jnoller@gmail.com>
2015-02-15 13:26:29 -0600

    proper version option

--- rackcli/rackcli.py
+++ rackcli/rackcli.py
@@ -57,15 +57,8 @@ class ComplexCLI(click.MultiCommand):
         return mod.cli
 
 
-def print_version(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    # how do I get this from __init__.py fml
-    click.echo('0.1.1')
-    ctx.exit()
-
-
 @click.command(cls=ComplexCLI)
+@click.version_option()
 @click.option('--debug', '-d', is_flag=True, default=False)
 @click.option('--no-verify-ssl', is_flag=True, default=False,
               help='Disable SSL - not recommended, considered harmful')
@@ -78,8 +71,6 @@ def print_version(ctx, param, value):
               help='Use a specific profile from your credential file.')
 @click.option('--region', '-r', required=False,
               help='The region to use. Overrides config/env settings.')
-@click.option('--version', is_flag=True, callback=print_version,
-              expose_value=False, is_eager=True)
 @pass_ctx
 def cli(ctx, debug, no_verify_ssl, config_file, output, profile, region):
     """ rackcli [options] <command> <subcommand> [parameters] """
