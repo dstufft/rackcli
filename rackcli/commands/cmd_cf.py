def9b96c489762ec67214eaac0f6c44c164c278f
Jesse Noller <jnoller@gmail.com>
2015-02-16 15:30:40 -0600

    use docstrings

--- rackcli/commands/cmd_cf.py
+++ rackcli/commands/cmd_cf.py
@@ -25,10 +25,12 @@ def cli(ctx):
 """
 Cloudfile Level Account things
 """
-@cli.command(name='metadata-account',
-             help='Get metadata about your Cloud Files Account')
+
+
+@cli.command(name='metadata-account')
 @pass_ctx
 def metatadata_account(ctx):
+    """ Retrieve Account-Level Cloud Files Metadata """
     conn = auth.conn(ctx)
     res = conn.object_store.get_account_metadata()
     for e in res.items():
@@ -39,9 +41,12 @@ Container level operations
 """
 
 
-@cli.command(name='containers', help='List all Cloud Files containers')
+@cli.command(name='containers')
 @pass_ctx
 def list_containers(ctx):
+    """List all Cloud Files containers for your specified region. If not in
+    interactive mode, these are printed to stdout; interactive mode will
+    paginate the output for you. """
     conn = auth.conn(ctx)
     if ctx.interactive:
         click.echo_via_pager('\n'.join('Container: %s' % c.name
@@ -52,38 +57,44 @@ def list_containers(ctx):
             click.echo(container.name)
 
 
-@cli.command(name='create-container', help='Create a container')
+@cli.command(name='create-container')
 @click.argument('containername')
 @pass_ctx
 def create_container(ctx, containername):
+    """ Create a Cloud Files container """
     conn = auth.conn(ctx)
     conn.object_store.create_container(containername)
 
 
-@cli.command(name='delete-container', help='Delete a container')
+@cli.command(name='delete-container')
 @click.argument('containername')
 @pass_ctx
 def delete_container(ctx, containername):
+    """ Delete the specified container - note, if the container is not empty
+    this operation will fail. See also bulkdelete-container for recursive
+    with container removal. """
     conn = auth.conn(ctx)
     conn.object_store.delete_container(containername)
 
 
-@cli.command(name='metadata-container', help='Get metadata about a container')
+@cli.command(name='metadata-container')
 @click.argument('containername')
 @pass_ctx
 def metatadata_container(ctx, containername):
+    """Get the metadata about a container."""
     conn = auth.conn(ctx)
     res = conn.object_store.get_container_metadata(containername)
     for e in res.items():
         click.echo('%s: %s' % (e[0], e[-1]))
 
 
-@cli.command(name='bulkdelete-container',
-             help='Recursively delete a container with the bulk delete command'
-             )
+@cli.command(name='bulkdelete-container')
 @click.argument('containername')
 @pass_ctx
 def bulkddelete_container(ctx, containername):
+    """ Recursively deletes a container with the bulk delete command, all
+    objects in the target container will be deleted as well as the container
+    itself. """
     conn = auth.conn(ctx)
     conn.object_store.bulk_delete(containername)
     conn.object_store.delete_container(containername)
@@ -93,11 +104,13 @@ Object level operations
 """
 
 
-@cli.command(name='objects',
-             help='List all objects in a Cloud Files container')
+@cli.command(name='objects')
 @click.argument('containername', required=True)
 @pass_ctx
 def list_objects(ctx, containername):
+    """List all Cloud Files objects in the specified container. If not in
+    interactive mode, these are printed to stdout; interactive mode will
+    paginate the output for you. """
     conn = auth.conn(ctx)
     if ctx.interactive:
         click.echo_via_pager('\n'.join('Container: %s' % c.name
@@ -109,12 +122,14 @@ def list_objects(ctx, containername):
             click.echo(object.name)
 
 
-@cli.command(name='metadata-object', help='Get metadata about an object')
+@cli.command(name='metadata-object')
 @click.argument('containername', required=True)
 @click.argument('objectname', required=True)
 @pass_ctx
-def metatadat_object(ctx, containername, objectname):
-
+def metatadata_object(ctx, containername, objectname):
+    """Get the metadata about a specific object in the container, note that
+    objects are stored container/<virtual path> - for example container/my/file
+    the object identifier is my/file."""
     # Hack around SDK interface
     oobj = obj.Object({"container": containername, "name": objectname})
     conn = auth.conn(ctx)
@@ -123,11 +138,12 @@ def metatadat_object(ctx, containername, objectname):
         click.echo('%s: %s' % (e[0], e[-1]))
 
 
-@cli.command(name='delete-object', help='Delete an object from a container')
+@cli.command(name='delete-object')
 @click.argument('containername', required=True)
 @click.argument('objectname', required=True)
 @pass_ctx
 def delete_object(ctx, containername, objectname):
+    """Delete target object from the specified container"""
     # Hack around SDK interface
     oobj = obj.Object({"container": containername, "name": objectname})
     conn = auth.conn(ctx)
@@ -147,12 +163,13 @@ def copy_object(ctx, containername, objectname):
 """
 
 
-@cli.command(name='save-object', help='Save an object from a container')
+@cli.command(name='save-object')
 @click.argument('containername', required=True)
 @click.argument('objectname', required=True)
 @click.argument('filepath', required=True)
 @pass_ctx
 def save_object(ctx, containername, objectname, filepath):
+    """ Save the specified object to a local file (filepath) """
     # Hack around SDK interface
     oobj = obj.Object({"container": containername, "name": objectname})
     conn = auth.conn(ctx)
@@ -160,13 +177,14 @@ def save_object(ctx, containername, objectname, filepath):
     conn.object_store.save_object(oobj, filepath)
 
 
-@cli.command(name='create-object',
-             help='Save a local file to the remote container & name/path')
+@cli.command(name='create-object')
 @click.argument('containername', required=True)
 @click.argument('objectname', required=True)
 @click.argument('inputfile', required=True, type=click.File('rb', lazy=False))
 @pass_ctx
 def create_object(ctx, containername, objectname, inputfile):
+    """Upload a local file to the remote container with the specified
+    objectname """
     # Hack around SDK interface
     oobj = obj.Object({"container": containername, "name": objectname})
     conn = auth.conn(ctx)
@@ -174,13 +192,14 @@ def create_object(ctx, containername, objectname, inputfile):
     conn.object_store.create_object(inputfile, oobj)
 
 
-@cli.command(name='data-object',
-             help='Advanced: Get raw data from object from a container, \
-prints to stdout')
+@cli.command(name='data-object')
 @click.argument('containername', required=True)
 @click.argument('objectname', required=True)
 @pass_ctx
-def data_object(ctx, containername, objectname, ):
+def data_object(ctx, containername, objectname):
+    """Advanced: Get raw data from object from a container this data
+    prints to stdout. This means if it's binary data you're going to have a
+    bad time."""
     # Hack around SDK interface
     oobj = obj.Object({"container": containername, "name": objectname})
     conn = auth.conn(ctx)
