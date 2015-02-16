cb84b856a942c06a56100315a9e52f2524dfedc2
Jesse Noller <jnoller@gmail.com>
2015-02-15 19:02:40 -0600

    container commands

--- rackcli/commands/cmd_files.py
+++ rackcli/commands/cmd_files.py
@@ -21,7 +21,7 @@ def cli(ctx):
     pass
 
 
-@cli.command(name='containers')
+@cli.command(name='containers', help='List all Cloud Files containers')
 @pass_ctx
 def containers_list(ctx):
 
@@ -33,3 +33,42 @@ def containers_list(ctx):
     else:
         for container in conn.object_store.containers():
             click.echo(container.name)
+
+
+@cli.command(name='container_create', help='Create a container')
+@click.argument('containername')
+@pass_ctx
+def container_create(ctx, containername):
+    conn = auth.conn(ctx)
+    conn.object_store.create_container(containername)
+
+
+@cli.command(name='container_delete', help='Delete a container')
+@click.argument('containername')
+@pass_ctx
+def container_delete(ctx, containername):
+    conn = auth.conn(ctx)
+    conn.object_store.create_container(containername)
+
+
+@cli.command(name='container_metadata', help='Get metadata about a container')
+@click.argument('containername')
+@pass_ctx
+def container_metadata(ctx, containername):
+    conn = auth.conn(ctx)
+    res = conn.object_store.get_container_metadata(containername)
+    for e in res.items():
+        click.echo('%s: %s' % (e[0], e[-1]))
+
+
+
+"""
+conn.object_store.bulk_delete             conn.object_store.get_object_data
+conn.object_store.get_object_metadata
+conn.object_store.copy_object             conn.object_store.objects
+conn.object_store.save_object
+conn.object_store.create_object           conn.object_store.session
+conn.object_store.delete_container        conn.object_store.set_account_metadata
+conn.object_store.delete_object
+conn.object_store.get_account_metadata    conn.object_store.set_object_metadata
+"""
